0b0a26c6b1df5624135133e23f123f7c6e2cd2ad
skywind3000 <skywind3000@163.com>
2022-02-03 23:43:10 +0800

    initial commit

---
+++ source/PyStand.cpp
@@ -0,0 +1,25 @@
+//=====================================================================
+//
+// PyStand.cpp - 
+//
+// Created by skywind on 2022/02/03
+// Last Modified: 2022/02/03 23:39:54
+//
+//=====================================================================
+#include "PyStand.h"
+
+
+
+
+
+//! flag: -static
+//! src: 
+//! mode: win
+//! int: objs
+int main(void)
+{
+	MessageBoxA(NULL, "Hello, World !!", "DD", MB_OK);
+	return 0;
+}
+
+
